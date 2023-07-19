0443930db8b4f39fd75f4f109a8b14e7da0e2a59
pms96 <55533643+pms96@users.noreply.github.com>
2023-07-19 07:38:48 +0200

    guie start on readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,23 +20,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
             
-        // DB::table('users')->insert([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@admin.com',
-        //     'email_verified_at' => now(),
-        //     'password' => Hash::make('admin'),
-        //     'two_factor_secret' => null,
-        //     'two_factor_recovery_codes' => null,
-        //     'remember_token' => Str::random(10),
-        //     'profile_photo_path' => null,
-        //     'current_team_id' => null,
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin'),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => 1,
+        ]);
 
-        // \App\Models\User::factory(50)->create();
-        // Product::factory(50)->create();
+        DB::table('teams')->insert([
+            'name' => 'Admin',
+            'user_id' => 1,
+            'personal_team' => true,
+        ]);
 
+        \App\Models\User::factory(50)->create();
 
-
+        Product::factory(50)->create();
 
     }
 }
